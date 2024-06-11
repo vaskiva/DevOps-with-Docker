46569a5eb76a4ee8ab02613d56e2c5a6b83de0bd
Tiina <tiina.vaskivaara@gmail.com>
2024-06-11 12:48:22 +0300

    changed a tag for build and push

--- exercise_3.1/watchtower.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-    --name watchtower \
-    --env WATCHTOWER_POLL_INTERVAL=30 \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    containrrr/watchtower \
-    vaskivaara/exercise3_1
